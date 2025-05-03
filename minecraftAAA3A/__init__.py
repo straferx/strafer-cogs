16398dd100320b476ea50195a1e5b4c627080ae0
straferx <65540032+straferx@users.noreply.github.com>
2025-05-03 23:04:45 +0500

    Update __init__.py

--- minecraftAAA3A/__init__.py
+++ minecraftAAA3A/__init__.py
@@ -36,7 +36,7 @@ del AAA3A_utils
 from redbot.core.bot import Red  # isort:skip
 from redbot.core.utils import get_end_user_data_statement
 
-from .minecraft import MinecraftAAA3A
+from .minecraft import Minecraft
 
 __red_end_user_data_statement__ = get_end_user_data_statement(file=__file__)
 

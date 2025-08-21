d191709a2543988fd2c9682beb0a86132ecffbc8
straferx <65540032+straferx@users.noreply.github.com>
2025-08-21 19:19:02 +0500

    Update __init__.py

--- wordlev2/__init__.py
+++ wordlev2/__init__.py
@@ -36,11 +36,11 @@ del AAA3A_utils
 from redbot.core.bot import Red  # isort:skip
 from redbot.core.utils import get_end_user_data_statement
 
-from .wordlegame import WordleGame
+from .wordlev2 import Wordlev2
 
 __red_end_user_data_statement__ = get_end_user_data_statement(file=__file__)
 
 
 async def setup(bot: Red) -> None:
-    cog = WordleGame(bot)
+    cog = Wordlev2(bot)
     await bot.add_cog(cog)

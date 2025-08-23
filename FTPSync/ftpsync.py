48f73193cc4ac1bc85b31a68c8786435aaecace6
straferx <65540032+straferx@users.noreply.github.com>
2025-08-23 14:31:06 +0500

    1

--- FTPSync/ftpsync.py
+++ FTPSync/ftpsync.py
@@ -9,7 +9,7 @@ import zipfile
 from typing import List, Optional
 import os
 
-class ftpsync(commands.Cog):
+class FtpSync(commands.Cog):
     """FTP Sync cog for backing up files from FTP server to Discord channels."""
 
     def __init__(self, bot: Red):
